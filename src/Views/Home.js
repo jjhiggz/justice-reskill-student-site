228b3b55c0e3639fa6aef1b6898c40f63c8613d7
jjhiggz <jonathan.higger@gmail.com>
2021-03-20 22:36:29 -0600

    added home message for testing

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <h1>Home</h1>;
+	return (
+		<h1>
+			Nothing is being done with the home page right now, please explore the
+			other routes
+		</h1>
+	);
 }
